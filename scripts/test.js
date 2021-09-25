25cc55efbee82484c1afd31b202996215d808729
brock <brocksmedley@gmail.com>
2021-09-25 15:33:03 -0700

    add stake, unstake, rewards to test

--- scripts/test.js
+++ scripts/test.js
@@ -34,29 +34,87 @@ const logBalances = async () => {
     // get admin balances
     const adminTokenBalance = await stakingTokenContractGeneric.balanceOf(adminSigner.address);
     const adminEthBalance = await provider.getBalance(adminSigner.address);
+    const adminTokenStake = await stakingTokenContractGeneric.stakeOf(adminSigner.address);
+    const adminTokenRewards = await stakingTokenContractAdmin.rewardOf(adminSigner.address);
 
     // get victim balances
     const victimTokenBalance = await stakingTokenContractGeneric.balanceOf(victimSigner.address);
     const victimEthBalance = await provider.getBalance(victimSigner.address);
+    const victimTokenStake = await stakingTokenContractGeneric.stakeOf(victimSigner.address);
+    const victimTokenRewards = await stakingTokenContractVictim.rewardOf(victimSigner.address);
 
     // log 'em
-    console.log(`Admin Balance (BORK):\t${formatEther(adminTokenBalance)}`);
-    console.log(`Admin Balance (ETH):\t${formatEther(adminEthBalance)}`);
+    console.log(`Admin Balance  (BORK):\t${formatEther(adminTokenBalance)}`);
+    console.log(`Admin Balance   (ETH):\t${formatEther(adminEthBalance)}`);
+    console.log(`Admin Stake    (BORK):\t${formatEther(adminTokenStake)}`);
+    console.log(`Admin Rewards  (BORK):\t${formatEther(adminTokenRewards)}`);
+    console.log("---------------------");
     console.log(`Victim Balance (BORK):\t${formatEther(victimTokenBalance)}`);
-    console.log(`Victim Balance (ETH):\t${formatEther(victimEthBalance)}`);
+    console.log(`Victim Balance  (ETH):\t${formatEther(victimEthBalance)}`);
+    console.log(`Victim Stake   (BORK):\t${formatEther(victimTokenStake)}`);
+    console.log(`Victim Rewards (BORK):\t${formatEther(victimTokenRewards)}`);
+
+    return {
+        admin: {
+            eth: adminEthBalance,
+            token: adminTokenBalance,
+            stake: adminTokenStake,
+            rewards: adminTokenRewards,
+        },
+        victim: {
+            eth: victimEthBalance,
+            token: victimTokenBalance,
+            stake: victimTokenStake,
+            rewards: victimTokenRewards,
+        }
+    };
 }
 
 async function main() {
     console.log(`Victim Address:\t${victimSigner.address}\n`);
-    await logBalances();
+    const startBalances = await logBalances();
 
-    // transfer 1000 tokens into victim account from admin
-    const transferBorkRes = await stakingTokenContractAdmin.transfer(victimSigner.address, ETHER.mul(1000));
+    if (startBalances.victim.eth.lt(ETHER.mul(1))) {
+        // transfer 0.1 ETH into victim account from admin (hardhat admin has 1000 ETH)
+        await adminSigner.sendTransaction({
+            value: ETHER.div(10),
+            to: victimSigner.address,
+        });
+        console.log("Transferred 1 ETH to victim account to pay for transactions");
+    }
+
+    // transfer 1000 tokens into victim account from admin (admin gets all tokens on contract deploy)
+    const tokenAmount = ETHER.mul(1000);
+    const transferBorkRes = await stakingTokenContractAdmin.transfer(victimSigner.address, tokenAmount);
     console.log(transferBorkRes && "\nTransferred 1000 BORK to Victim account.\n");
 
     await logBalances();
 
-    // TODO: stake BORK from victim's account
+    // stake BORK from victim's account
+    const stakeBorkRes = await stakingTokenContractVictim.createStake(tokenAmount);
+    console.log(stakeBorkRes && "\nStaked 1000 BORK from Victim account.\n");
+
+    await logBalances();
+
+    // distribute rewards to stakeholders
+    const distributeRewardsRes = await stakingTokenContractAdmin.distributeRewards();
+    console.log(distributeRewardsRes && "\nAdmin distributed rewards.\n");
+
+    await logBalances();
+
+    // claim rewards from victim account
+    const claimRewardsRes = await stakingTokenContractVictim.withdrawReward();
+    console.log(claimRewardsRes && "\nVictim claimed rewards.\n");
+
+    await logBalances();
+
+    // unstake BORK from victim's account
+    const unstakeBorkRes = await stakingTokenContractVictim.removeStake(tokenAmount);
+    console.log(unstakeBorkRes && "\nUnstaked 1000 BORK from Victim account.\n");
+
+    await logBalances();
+
+
 }
 
 main()
